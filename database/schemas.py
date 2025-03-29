e81931309d6a71716ada8c30112c02618855c3bb
Pedro de Castro Kurtz <pedrokurtz.01@outlook.com>
2025-03-29 20:29:51 -0300

    criando schemas para as requisicoes

---
+++ database/schemas.py
@@ -0,0 +1,29 @@
+# Importacao das bibliotecas usadas
+
+from flask_marshmallow import Marshmallow
+from custom_models import User, Order, TradeReport
+
+# Definicao de variavel para chamar o Marshmallow e suas funcoes
+ma = Marshmallow()
+
+# Schema para a tabela de User
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        incluir_relacao = True
+        instance = True
+
+# Schema para a tabela de Order
+class OrderSchema(ma.SQLALchemyAutoSchema):
+    class Meta:
+        model = Order
+        incluir_fk = True
+        incluir_relacao = True
+        instance = True
+
+# Schema para a tabela de TradeReport
+class TradeReportSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = TradeReport
+        include_fk = True
+        instance = True
